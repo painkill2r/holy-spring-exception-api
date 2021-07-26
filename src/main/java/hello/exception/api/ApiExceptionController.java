f080acea9cc1653df62694d24ef1f3391cdda7e7
wdEffort <yoman3011@gmail.com>
2021-07-26 22:27:58 +0900

    스프링이 제공하는 ExceptionResolver - DefaultHandlerExceptionResolver 사용

--- src/main/java/hello/exception/api/ApiExceptionController.java
+++ src/main/java/hello/exception/api/ApiExceptionController.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -36,9 +37,8 @@ public class ApiExceptionController {
     }
 
     /**
-     * @ResponseStatus를 사용
-     *
      * @return
+     * @ResponseStatus를 사용
      */
     @GetMapping("/api/response-status-ex1")
     public String responseStatusEx1() {
@@ -53,6 +53,11 @@ public class ApiExceptionController {
     @GetMapping("/api/response-status-ex2")
     public String responseStatusEx12() {
         throw new ResponseStatusException(HttpStatus.NOT_FOUND, "error.bad", new IllegalArgumentException());
+    }
+
+    @GetMapping("/api/default-handler-ex")
+    public String defaultException(@RequestParam Integer data) {
+        return "ok";
     }
 
     @Data
