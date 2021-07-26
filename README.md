7a0c2624a5da7c18d907eca714e8070119a0aff1
wdEffort <yoman3011@gmail.com>
2021-07-26 12:18:58 +0900

    API 예외 처리 - 서블릿 오류 페이지 처리 방식의 API 예외 처리

--- README.md
+++ README.md
@@ -17,3 +17,10 @@ Spring Boot에서 API에 대한 예외 처리 학습
         - Lombok
         - Validation
 4. Build Tools : Gradle
+
+## API 예외 처리
+
+1. 지금까지 HTML을 사용한 오류 페이지는 단순히 고객에게 오류 화면을 보여주고 끝이 났다.
+    - 4xx, 5xx HTTML 오류 페이지
+2. 하지만 API는 각 오류 상황에 맞는 `오류 응답 스펙`을 정하고, JSON으로 데이터를 내려주어야 한다.
+    - 클라이언트는 정상 요청이든, 오류 요청이든 JSON이 반환되기를 기대한다.
--- src/main/java/hello/exception/servlet/ErrorPageController.java
+++ src/main/java/hello/exception/servlet/ErrorPageController.java
@@ -1,11 +1,18 @@
 package hello.exception.servlet;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -19,7 +26,7 @@ public class ErrorPageController {
     public static final String ERROR_SERVLET_NAME = "javax.servlet.error.servlet_name";
     public static final String ERROR_STATUS_CODE = "javax.servlet.error.status_code";
 
-    @GetMapping("/error-page/404")
+    @RequestMapping("/error-page/404")
     public String errorPage404(HttpServletRequest request, HttpServletResponse response) {
         log.info("error page 404");
 
@@ -28,13 +35,36 @@ public class ErrorPageController {
         return "error-page/404";
     }
 
-    @GetMapping("/error-page/500")
+    @RequestMapping("/error-page/500")
     public String errorPage500(HttpServletRequest request, HttpServletResponse response) {
         log.info("error page 500");
 
         printErrorInfo(request);
 
         return "error-page/500";
+    }
+
+    /**
+     * API 예외 처리
+     * <p>
+     * 클라이언트가 보내는 Accept 타입이 "application/json"인 경우 동작
+     * 동일한 URL 매핑정보가 있더라도 우선수위가 높다.
+     *
+     * @param request
+     */
+    @RequestMapping(value = "/error-page/500", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Map<String, Object>> errorPage500Api(HttpServletRequest request, HttpServletResponse response) {
+        log.info("API errorPage 500");
+
+        Map<String, Object> result = new HashMap<>();
+        Exception ex = (Exception) request.getAttribute(ERROR_EXCEPTION); //예외 정보
+
+        result.put("status", request.getAttribute(ERROR_STATUS_CODE)); //예외 코드
+        result.put("message", ex.getMessage()); //예외 메시지
+
+        Integer statusCode = (Integer) request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        return new ResponseEntity<>(result, HttpStatus.valueOf(statusCode));
     }
 
     /**
